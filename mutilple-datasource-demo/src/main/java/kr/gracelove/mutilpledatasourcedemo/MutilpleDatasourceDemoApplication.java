a9fa5ac369afc21296a887792d7cbfb2e2a1a387
MorrisHong <govlmo91@gmail.com>
2020-12-31 22:11:34 +0900

    init

---
+++ mutilple-datasource-demo/src/main/java/kr/gracelove/mutilpledatasourcedemo/MutilpleDatasourceDemoApplication.java
@@ -0,0 +1,13 @@
+package kr.gracelove.mutilpledatasourcedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutilpleDatasourceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutilpleDatasourceDemoApplication.class, args);
+	}
+
+}
---
+++ mutilple-datasource-demo/src/test/java/kr/gracelove/mutilpledatasourcedemo/MutilpleDatasourceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.gracelove.mutilpledatasourcedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutilpleDatasourceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
